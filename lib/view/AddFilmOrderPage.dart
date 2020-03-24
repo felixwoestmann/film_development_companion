6fd286b3bee595882fe006a392d756a0c4ef9021
Felix Wöstmann <f.wosti@gmail.com>
2020-03-24 09:53:21 +0100

    Added new Page to add Filmorders

---
+++ lib/view/AddFilmOrderPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AddFilmOrderPage extends StatefulWidget {
+  AddFilmOrderPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _AddFilmOrderPageState createState() => _AddFilmOrderPageState();
+}
+
+class _AddFilmOrderPageState extends State<AddFilmOrderPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {}
+}
