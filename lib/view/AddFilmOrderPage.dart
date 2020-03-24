3bfe749f44aecd8de8a2cde364bd8f16291a90f9
Felix Wöstmann <f.wosti@gmail.com>
2020-03-24 10:30:44 +0100

    Added Second View with Store chooser

--- lib/view/AddFilmOrderPage.dart
+++ lib/view/AddFilmOrderPage.dart
@@ -1,20 +1,61 @@
+import 'package:filmdevelopmentcompanion/model/DmDeStoreModel.dart';
+import 'package:filmdevelopmentcompanion/model/StoreModel.dart';
 import 'package:flutter/material.dart';
 
 class AddFilmOrderPage extends StatefulWidget {
-  AddFilmOrderPage({Key key, this.title}) : super(key: key);
 
-  final String title;
+  final String title="Add Film";
 
   @override
   _AddFilmOrderPageState createState() => _AddFilmOrderPageState();
 }
 
 class _AddFilmOrderPageState extends State<AddFilmOrderPage> {
+  List<StoreModel> storeModels = new List();
+
   @override
   void initState() {
     super.initState();
+    storeModels.add(new DmDeStoreModel());
   }
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, position) {
+          return Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(12.0, 20.0, 0.0, 10.0),
+                        child: Text(
+                          storeModels[position].providerName,
+                          style: TextStyle(fontSize: 22.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(
+                height: 2.0,
+                color: Colors.grey,
+              )
+            ],
+          );
+        },
+        itemCount: storeModels.length,
+      ),
+    );
+  }
 }
--- lib/view/FilmOrderOverviewPage.dart
+++ lib/view/FilmOrderOverviewPage.dart
@@ -1,4 +1,5 @@
 import 'package:filmdevelopmentcompanion/model/FilmDevelopmentOrder.dart';
+import 'package:filmdevelopmentcompanion/view/AddFilmOrderPage.dart';
 import 'package:flutter/material.dart';
 import '../model/DmDeStoreModel.dart';
 import '../model/StoreModel.dart';
@@ -50,70 +51,79 @@ class _FilmOrderOverviewPageState extends State<FilmOrderOverviewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: RefreshIndicator(
-            child: ListView.builder(
-              itemBuilder: (context, position) {
-                return Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  12.0, 12.0, 12.0, 6.0),
-                              child: Text(
-                                filmOrders[position]
-                                    .insertionDate
-                                    .toIso8601String(),
-                                style: TextStyle(fontSize: 15.0),
-                              ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+          child: ListView.builder(
+            itemBuilder: (context, position) {
+              return Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                                12.0, 12.0, 12.0, 6.0),
+                            child: Text(
+                              filmOrders[position]
+                                  .insertionDate
+                                  .toIso8601String(),
+                              style: TextStyle(fontSize: 15.0),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  12.0, 12.0, 12.0, 6.0),
-                              child: Text(
-                                filmOrders[position]
-                                    .latestFilmDevelopmentStatusSummaryText,
-                                style: TextStyle(fontSize: 22.0),
-                              ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                                12.0, 12.0, 12.0, 6.0),
+                            child: Text(
+                              filmOrders[position]
+                                  .latestFilmDevelopmentStatusSummaryText,
+                              style: TextStyle(fontSize: 22.0),
                             ),
-                            Padding(
-                                padding: const EdgeInsets.fromLTRB(
-                                    12.0, 6.0, 12.0, 12.0),
-                                child: Row(
-                                  children: <Widget>[
-                                    Text(
-                                      filmOrders[position].orderNumber,
-                                      style: TextStyle(
-                                          fontSize: 22.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Padding(
-                                        padding: const EdgeInsets.fromLTRB(
-                                            8.0, 0.0, 0.0, 0.0)),
-                                    Text(filmOrders[position].storeId,
-                                        style: TextStyle(fontSize: 18.0))
-                                  ],
-                                )),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: 2.0,
-                      color: Colors.grey,
-                    )
-                  ],
-                );
-              },
-              itemCount: filmOrders.length,
-            ),
-            onRefresh: updateAllOrders));
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  12.0, 6.0, 12.0, 12.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    filmOrders[position].orderNumber,
+                                    style: TextStyle(
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          8.0, 0.0, 0.0, 0.0)),
+                                  Text(filmOrders[position].storeId,
+                                      style: TextStyle(fontSize: 18.0))
+                                ],
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    height: 2.0,
+                    color: Colors.grey,
+                  )
+                ],
+              );
+            },
+            itemCount: filmOrders.length,
+          ),
+          onRefresh: updateAllOrders),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddFilmOrderPage()));
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.yellowAccent,
+      ),
+    );
   }
 }
