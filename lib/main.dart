84f8fffcfb2432f0ebea7b43b12a38adfdf97174
Felix Wöstmann <f.wosti@gmail.com>
2020-03-25 17:25:24 +0100

    Changed color paletter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:filmdevelopmentcompanion/view/FilmOrderOverviewPage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(FilmDevCompanionApp());
 
@@ -10,11 +11,12 @@ class FilmDevCompanionApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        brightness: Brightness.dark,
+        accentColor: Color.fromARGB(255, 255, 235, 59),
+        primaryColor: Color.fromARGB(255, 0, 0, 1),
+//fontFamily: 'Roboto',
       ),
       home: FilmOrderOverviewPage(title: 'Film Development Companion'),
     );
   }
 }
-
-
