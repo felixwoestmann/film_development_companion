997e573f80bc8abfc7171ba090980ad67417765d
Felix Wöstmann <f.wosti@gmail.com>
2020-03-26 15:47:52 +0100

    Updated DmDeAddFilm Page

--- lib/view/DmDeAddFilmOrderPage.dart
+++ lib/view/DmDeAddFilmOrderPage.dart
@@ -1,4 +1,6 @@
 import 'package:filmdevelopmentcompanion/model/DmDeStoreModel.dart';
+import 'package:filmdevelopmentcompanion/model/FilmDevelopmentOrder.dart';
+import 'package:filmdevelopmentcompanion/model/FilmDevelopmentOrderDataHolder.dart';
 import 'package:filmdevelopmentcompanion/model/StoreModel.dart';
 import 'package:flutter/material.dart';
 
@@ -11,9 +13,28 @@ class DmDeAddFilmOrderPage extends StatefulWidget {
 
 class _DmDeAddFilmOrderPageState extends State<DmDeAddFilmOrderPage> {
   //TODO use Form https://api.flutter.dev/flutter/widgets/Form-class.html
+
+  final orderIdTextController = TextEditingController();
+  final storeIdTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    orderIdTextController.dispose();
+    storeIdTextController.dispose();
+    super.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
+  }
+
+  void addFilmOrder() {
+    FilmDevelopmentDataHolder.instance.addFilmOrder(new FilmDevelopmentOrder(
+        new DmDeStoreModel(),
+        orderIdTextController.text,
+        storeIdTextController.text));
   }
 
   @override
@@ -28,6 +49,7 @@ class _DmDeAddFilmOrderPageState extends State<DmDeAddFilmOrderPage> {
             padding: const EdgeInsets.fromLTRB(15.0, 30.0, 15.0, 10.0),
             child: TextField(
               autocorrect: false,
+              controller: storeIdTextController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 hintText: "Store ID",
@@ -39,6 +61,7 @@ class _DmDeAddFilmOrderPageState extends State<DmDeAddFilmOrderPage> {
             padding: const EdgeInsets.fromLTRB(15.0, 30.0, 15.0, 10.0),
             child: TextField(
               autocorrect: false,
+              controller: orderIdTextController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 hintText: "Order ID",
@@ -49,9 +72,8 @@ class _DmDeAddFilmOrderPageState extends State<DmDeAddFilmOrderPage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.save),
-        backgroundColor: Colors.yellowAccent,
+        onPressed: addFilmOrder,
+        child: Icon(Icons.check),
       ),
     );
   }
